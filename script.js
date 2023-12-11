2fa6429c767c6c116b05e2ed7d0a554861e9c811
Shayan-Nawab <shayannawab@gmail.com>
2023-12-11 08:21:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let lastKeyPressed = '';
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'w') {
+        window.open('https://wikipedia.org/', '_blank');
+    } else if (event.key === '4') {
+        if (event.ctrlKey) {
+            window.open('https://tetris.com/', '_blank');
+        } else {
+            lastKeyPressed = '4';
+        }
+    } else if (event.key === 'g' && lastKeyPressed === '4') {
+        window.open('https://openai.com/', '_blank');
+        lastKeyPressed = ''; // Reset the last key pressed
+    } else {
+        lastKeyPressed = ''; // Reset the last key pressed for any other keys
+    }
+});
+
