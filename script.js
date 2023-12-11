9ed25f1a75810e35c1fa96989f3f60bdb4b86b2d
Shayan-Nawab <shayannawab@gmail.com>
2023-12-11 08:45:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-let lastKeyPressed = '';
+const gifList = ['image1.gif', 'image2.gif', 'image3.gif']; // Add more as needed
+
+function getRandomGif() {
+    const randomIndex = Math.floor(Math.random() * gifList.length);
+    return gifList[randomIndex];
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('randomGif').src = 'Images/' + getRandomGif();
+});
+
+let fourPressed = false;
+let sPressed = false;
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'w') {
         window.location.href = 'https://wikipedia.org/';
+    } else if (event.key === '5' && event.ctrlKey) {
+        window.location.href = 'https://tetris.com/';
     } else if (event.key === '4') {
-        if (event.ctrlKey) {
-            window.open('https://tetris.com/', '_blank');
-        } else {
-            lastKeyPressed = '4';
+        fourPressed = true;
+    } else if (event.key === 's') {
+        sPressed = true;
+    } else if (event.key === 'g') {
+        if (fourPressed) {
+            window.location.href = 'https://openai.com/';
+            fourPressed = false; // Reset the flag
         }
-    } else if (event.key === 'g' && lastKeyPressed === '4') {
-        window.open('https://openai.com/', '_blank');
-        lastKeyPressed = ''; // Reset the last key pressed
+    } else if (event.key === 'q') {
+        if (sPressed) {
+            window.location.href = 'https://courses.torontomu.ca/d2l/home/790896';
+            fourPressed = false; // Reset the flag
+        }
     } else {
-        lastKeyPressed = ''; // Reset the last key pressed for any other keys
+        fourPressed = false; // Reset the flag for any other keys
+        sPressed = false;
     }
 });
-
